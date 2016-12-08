681f30c023406e2ea9f738e6fa89e441571a88df
thomasRambaldi <t.rambaldi13@gmail.com>
2016-12-08 18:18:18 +0100

    Reglage du probleme au lancement du birds

--- refactoring/src/refactoring/game/Game.java
+++ refactoring/src/refactoring/game/Game.java
@@ -22,7 +22,6 @@ import refactoring.objects.Pig;
 import refactoring.point.Point;
 
 /*
- * REGLER LES PROBLEEMES DE CHANGEMENT DE NIVEAU !!!!
  * corriger les problemes de colisions avec les birds plus gros
  */
 public class Game extends JPanel implements Runnable, MouseListener, MouseMotionListener{
@@ -33,7 +32,7 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 	private final int SIZE_IMAGE_HUGE = 60;
 	private final int SLINGSHOTW = 60;
 	private final int SLINGSHOTH = 150;
-	
+
 	private Level[] levels;
 	private int currentLevel;
 	private int currentBird;
@@ -45,6 +44,8 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 	private int mouseX, mouseY, score;
 	private boolean gameOver;
 	private int windowWidth, windowHeight;
+
+	private double gravity;
 
 	public Game(int windowWidth, int windowHeight) throws IOException {
 		super();
@@ -59,7 +60,7 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 	}
 
 	public void init() throws IOException{
-		
+
 		currentBird = 0;
 		currentPig = 0;
 		selecting = true;
@@ -82,40 +83,84 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 		listObjects.add(lancePierre);
 
 		levels[0] = new Level(Difficulties.EASY, listBirds, listPigs, listObjects, ImageIO.read(new File("./res/nature.jpg")), 1);
-		
-		
+
+
 		//Liste de Pigs level 1
 		List<Pig> listPigs1 = new ArrayList<>();
 		Pig littlePig1 = new Pig(new Point( generatePigPosition(300, windowWidth-100), windowHeight - 150), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL,  ImageIO.read(new File("./res/pig_1.png")), false, 1, 1, 0.0);
 		Pig MediumPig1 = new Pig(new Point( generatePigPosition(300, windowWidth-100), windowHeight - 150), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL,  ImageIO.read(new File("./res/armor_pig.png")), false, 4, 3, 0.0);
 		listPigs1.add(littlePig1);
 		listPigs1.add(MediumPig1);
-		
+
 		//Liste d'oiseaux level 1
 		List<Bird> listBirds1 = new ArrayList<>(); 
 		Bird red1 = new Bird(new Point(initBirdX, initBirdY), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL, ImageIO.read(new File("./res/red.png")), true, 0, new Point(5, 5), 0.1);
 		Bird chuck1 = new Bird(new Point(windowWidth/6 - 50, windowHeight/1.3 + 30), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL, ImageIO.read(new File("./res/chuck.png")), true, 0, new Point(5, 5), 0.1);
-		Bird bomb1 = new Bird(new Point(windowWidth/6 - 50, windowHeight/1.3 + 30), SIZE_IMAGE_LARGE, SIZE_IMAGE_HUGE, ImageIO.read(new File("./res/bomb.png")), true, 0, new Point(5, 5), 0.1);
-		Bird terence1 = new Bird(new Point(windowWidth/6 - 50, windowHeight/1.3 + 30), SIZE_IMAGE_HUGE, SIZE_IMAGE_HUGE, ImageIO.read(new File("./res/terence.png")), true, 0, new Point(5, 5), 0.1);
+		Bird bomb1 = new Bird(new Point(windowWidth/6 - 50, windowHeight/1.3 + 30), SIZE_IMAGE_LARGE, SIZE_IMAGE_HUGE, ImageIO.read(new File("./res/bomb.png")), true, 0, new Point(5, 5), 0.2);
+		Bird terence1 = new Bird(new Point(windowWidth/6 - 50, windowHeight/1.3 + 30), SIZE_IMAGE_HUGE, SIZE_IMAGE_HUGE, ImageIO.read(new File("./res/terence.png")), true, 0, new Point(5, 5), 0.3);
 
 		listBirds1.add(red1);
 		listBirds1.add(chuck1);
 		listBirds1.add(bomb1);
 		listBirds1.add(terence1);
-		
+
 		levels[1] = new Level(Difficulties.EASY, listBirds1, listPigs1, listObjects, ImageIO.read(new File("./res/marais.jpg")), 1);
 
-		
+
 		gameOver = false;
 		addMouseListener(this);
 		addMouseMotionListener(this);
+	}
+
+	private void initForNextLevel() throws IOException {
+
+		if(currentLevel==0){
+			List<Bird> listBirds = new ArrayList<>(); 
+			Bird red = new Bird(new Point(initBirdX, initBirdY), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL, ImageIO.read(new File("./res/red.png")), true, 0, new Point(5, 5), 0.1);
+			listBirds.add(red);
+			List<Pig> listPigs = new ArrayList<>();
+			Pig littlePig = new Pig(new Point( generatePigPosition(300, windowWidth-100), windowHeight - 150), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL,  ImageIO.read(new File("./res/pig_1.png")), false, 1, 1, 0.0);
+			listPigs.add(littlePig);
+			levels[0].setListBirds(listBirds);
+			levels[0].setListPigs(listPigs);
+		}
+		if(currentLevel==1){
+			List<Pig> listPigs1 = new ArrayList<>();
+			Pig littlePig1 = new Pig(new Point( generatePigPosition(300, windowWidth-100), windowHeight - 150), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL,  ImageIO.read(new File("./res/pig_1.png")), false, 1, 1, 0.0);
+			Pig MediumPig1 = new Pig(new Point( generatePigPosition(300, windowWidth-100), windowHeight - 150), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL,  ImageIO.read(new File("./res/armor_pig.png")), false, 4, 3, 0.0);
+			listPigs1.add(littlePig1);
+			listPigs1.add(MediumPig1);
+
+			//Liste d'oiseaux level 1
+			List<Bird> listBirds1 = new ArrayList<>(); 
+			Bird red1 = new Bird(new Point(initBirdX, initBirdY), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL, ImageIO.read(new File("./res/red.png")), true, 0, new Point(5, 5), 0.1);
+			Bird chuck1 = new Bird(new Point(windowWidth/6 - 50, windowHeight/1.3 + 30), SIZE_IMAGE_NORMAL, SIZE_IMAGE_NORMAL, ImageIO.read(new File("./res/chuck.png")), true, 0, new Point(5, 5), 0.1);
+			Bird bomb1 = new Bird(new Point(windowWidth/6 - 50, windowHeight/1.3 + 30), SIZE_IMAGE_LARGE, SIZE_IMAGE_HUGE, ImageIO.read(new File("./res/bomb.png")), true, 0, new Point(5, 5), 0.2);
+			Bird terence1 = new Bird(new Point(windowWidth/6 - 50, windowHeight/1.3 + 30), SIZE_IMAGE_HUGE, SIZE_IMAGE_HUGE, ImageIO.read(new File("./res/terence.png")), true, 0, new Point(5, 5), 0.3);
+
+			listBirds1.add(red1);
+			listBirds1.add(chuck1);
+			listBirds1.add(bomb1);
+			listBirds1.add(terence1);
+
+			levels[1].setListBirds(listBirds1);
+			levels[1].setListPigs(listPigs1);
+		}
+
+
+		currentBird = 0;
+		currentPig = 0;
+		selecting = true;
+		initBirdX = (int) (windowWidth/6 - 30) ;
+		initBirdY = (int) (windowHeight/1.3 - 70) ;
+		gameOver = false;
 	}
 
 	public int generatePigPosition( int valueMin, int valueMax ){
 		Random ran = new Random();
 		return (valueMin + ran.nextInt(valueMax - valueMin));
 	}
-	
+
 	public void paint(Graphics g) {
 		super.paint(g);
 
@@ -123,14 +168,15 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 		drawBirds(g);
 		drawPigs(g);
 		drawObjectOfLevel(g);
-
+		Bird b = levels[currentLevel].getListBirds().get(currentBird);
 		if(selecting){
-			Bird b = levels[currentLevel].getListBirds().get(currentBird);
 			b.setPosition(new Point(mouseX-b.getWidth()/2, mouseY-b.getWidth()/2));
 			g.setColor(Color.RED);
 			g.drawLine( (int)initBirdX + b.getWidth()/2, 
-						(int)initBirdY + b.getWidth()/2, mouseX, mouseY);
+					(int)initBirdY + b.getWidth()/2, mouseX, mouseY);
+			
 		}
+		
 	}
 
 	public void drawObjectOfLevel(Graphics g) {
@@ -166,16 +212,16 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 			// un pas de simulation toutes les 10ms
 			try { Thread.currentThread().sleep(10); } catch(InterruptedException e) { }
 
-
+			
 			if(!selecting && !gameOver) {
 				Bird b = levels[currentLevel].getListBirds().get(currentBird);
 				b.updatePosition();				
-
+				System.out.println(b.getVelocity().getX()+ " - "+b.getVelocity().getY());
 				List<Pig> pigs= levels[currentLevel].getListPigs();
 				for(int i = 0 ; i < pigs.size() ; i++){
 					if(Point.distance(b.getPosition(), pigs.get(i).getPosition()) < 35  ){
 
-						selecting = true;
+
 
 						//Le cochons perd de la vie on change d'image
 						if( pigs.get(i).getLife() > 1){
@@ -185,16 +231,19 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 						else // le cochons disparait car plus de vie
 							pigs.remove(pigs.get(i));
 
-						// Si plus de birds disponible on stop
+						// Si plus de birds disponible ou le niveau est terminé on stop
+
 						if(currentBird==levels[currentLevel].getListBirds().size()-1)
 							stop();
-						else // sinon on passe au bird suivant
+						else{ // sinon on passe au bird suivant
+							selecting = true;
 							currentBird++;
+						}	
 					}
 				}
 
-				if(b.getPosition().getX() > initBirdX)
-					b.setGravity(0.15);
+				if(b.getPosition().getX()> initBirdX)
+					b.setGravity(gravity);
 
 				if(b.getPosition().getY() >  windowHeight - 150 && (b.getVelocity().getY() > 0.001) ){
 					b.getVelocity().setY(b.getVelocity().getY()*-0.8);
@@ -213,7 +262,7 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 				if(pigs.isEmpty()){
 					stop();
 				}
-				
+
 			}
 			// redessine
 			repaint();
@@ -243,25 +292,41 @@ public class Game extends JPanel implements Runnable, MouseListener, MouseMotion
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		if(gameOver) {
-			try {
-				init();
+
+			if(currentLevel+1 == levels.length)
+				currentLevel = 0;
+			else
 				currentLevel++;
+
+			try {
+				initForNextLevel();
 			} catch (IOException e1) {
+				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
+
 		} else if(selecting) {
 			Bird b = levels[currentLevel].getListBirds().get(currentBird);
-			Point init = new Point(initBirdX,initBirdY);
+			Point init = new Point(initBirdX+ b.getWidth()/2,initBirdY+ b.getWidth()/2);
 			Point mouse = new Point(mouseX,mouseY);
-			Point velocity = new Point((int) ((initBirdX - mouseX) / 20.0)*Point.distance(init, mouse)/200, 
-					(int) ((initBirdY - mouseY) / 20.0)*Point.distance(init, mouse)/200);
-			b.setGravity(0);
+			double x = (((int)initBirdX + b.getWidth()/2) - mouseX)/20;
+			double y = (((int)initBirdY + b.getWidth()/2) - mouseY   )/20;
+			double distance = Point.distance(init, mouse)/150;
+			Point velocity = new Point(x * distance , y * distance);
+					
+//					new Point((int) ((initBirdX - mouseX) / 20.0)/**Point.distance(init, mouse)/200*/, 
+//					(int) ((initBirdY - mouseY) / 20.0)/**Point.distance(init, mouse)/100*/);
+			gravity=b.getGravity();
+			b.setGravity(0.0001);
 			b.setVelocity(velocity);
+			System.out.println("BEFORE : " + b.getVelocity().getX()+ " - "+b.getVelocity().getY());
 			selecting = false;
 		}
 		repaint();		
 	}
-	
+
+
+
 	public Level[] getLevels() {
 		return levels;
 	}
